76f50b2dbb61793e01b4b2eeeabd5f2fb98b0c3e
Justin Bond <bond.justink@gmail.com>
2018-01-12 16:48:54 -0800

    Added app view and pulled in based on page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,18 +1,14 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
+@section('content')
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-    </head>
-    <body>
-        <div class="container">
-        	<h1>Contact Page</h1>
-        </div>
-    </body>
-</html>
+<h1>Contact Page</h1>
+
+@endsection
+
+
+@section('footer')
+
+<script type="text/javascript">alert('hello visitor')</script>
+
+@endsection
